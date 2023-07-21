83ae3ab093e97298614bea75c36b0d34f51b025e
Kirankp <jnvkirankp25522@gmail.com>
2023-07-22 02:16:31 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,137 @@
+export const projects = [
+  {
+    title: "NEWS-API",
+    desc: "This repository contains all the top news headlines from fetching data from news-api",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "red-600",
+      },
+      {
+        name: "Express",
+        color: "green-600",
+      },
+    ],
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwTIgH1CWbmK_LAPlHpFajPhD0c-DKpJzcKA&usqp=CAU",
+    link: "https://github.com/Kirankp25/News-API",
+    code: "https://github.com/Kirankp25/News-API",
+  },
+  {
+    title: "TODO Web-App",
+    desc: "This is a MyTODO site made for daily-use. Feel free to contribute to this project. ", 
+    tags: [
+      {
+        name: "HTML",
+        color: "red-600",
+      },
+      {
+        name: "Javascript",
+        color: "green-600",
+      },
+      {
+        name: "Bootstrap",
+        color: "yellow-600",
+      },
+      {
+        name: "CSS",
+        color: "blue-800",
+      },
+    ],
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVZTksUHB6lkzRtQFOOvwsKwBZR8E78Czpig&usqp=CAU",
+    link: "https://kirankp25.github.io/TODO",
+    code: "https://github.com/Kirankp25/TODO",
+  },
+  {
+    title: "Weather Web-App",
+    desc: "This repository contain the project : Weather of particular location in the world by using OpenWeatherApi, Which display live data of weather Report..",
+    tags: [
+      {
+        name: "React",
+        color: "red-600",
+      },
+      {
+        name: "Open-Api",
+        color: "green-600",
+      },
+      {
+        name: "Javascript",
+        color: "yellow-600",
+      },
+      {
+        name: "Bootstrap",
+        color: "blue-800",
+      },
+    ],
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xOTY0dQGm_rlbqgd_n6spaHQh0gMys-dug&usqp=CAU",
+    link: "https://myweather-report.netlify.app/",
+    code: "https://github.com/Kirankp25/Weather-Open-API-2.0",
+  },
+  {
+    title: "TripTogether",
+    desc: "Stock Market Website done with ReactJs as Frontend and Express as Backend. This websites gives practice for people those who want to start using stock market. It is based on realtime update of stock value.",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "red-600",
+      },
+      {
+        name: "ExpressJs",
+        color: "green-600",
+      },
+      {
+        name: "MongoDB",
+        color: "blue-800",
+      },
+    ],
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109032447783469097/image.png",
+    link: "https://stock-market-seven.vercel.app/",
+    code: "https://github.com/daniel-jebarson/Stock-Market",
+  },
+  {
+    title: "TripTogether",
+    desc: "Stock Market Website done with ReactJs as Frontend and Express as Backend. This websites gives practice for people those who want to start using stock market. It is based on realtime update of stock value.",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "red-600",
+      },
+      {
+        name: "ExpressJs",
+        color: "green-600",
+      },
+      {
+        name: "MongoDB",
+        color: "blue-800",
+      },
+    ],
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109032447783469097/image.png",
+    link: "https://stock-market-seven.vercel.app/",
+    code: "https://github.com/daniel-jebarson/Stock-Market",
+  },
+  {
+    title: "TinDog Web-App",
+    desc: "An app made with simple Html,CSS and Javascript to help people to sell and buy their pets dogs without intermediate. It also gives suggestions.",
+    tags: [
+     
+      {
+        name: "MaterialUI",
+        color: "green-600",
+      },
+      {
+        name: "CSS",
+        color: "yellow-600",
+      },
+      {
+        name: "Html",
+        color: "blue-800",
+      },
+      {
+        name: "Javascript",
+        color: "orange-600",
+      },
+    ],
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuYyaj2Hy_2RWFztLA7xBC1y3TAjVunfKj4A&usqp=CAU",
+    link: "https://celadon-unicorn-95623b.netlify.app/",
+    code: "https://github.com/Kirankp25/Tindog",
+  },
+];
